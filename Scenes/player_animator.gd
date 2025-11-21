77f3d40f01fd3eb17503ff310ee83ff31671383a
Medmav <medmav@icloud.com>
2025-11-20 21:24:01 -0600

    Slide animations added
    Yes yes

---
+++ Scenes/player_animator.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@export var player_controller : PlayerController
+@export var animation_player : AnimationPlayer
+@export var sprite : Sprite2D
+
+func _process(delta: float) -> void:
+	if player_controller.direction == 1 && player_controller.velocity.x > 0:
+		sprite.flip_h = false
+	elif player_controller.direction == -1 && player_controller.velocity.x < 0:
+		sprite.flip_h = true
+		
+	if player_controller.velocity.x:
+		animation_player.play("walk")
+	else:
+		animation_player.play("idle")
+		
+	if player_controller.sliding:
+		animation_player.play("slide")
--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -11,33 +11,35 @@ var sliding := false
 var jumping := false
 var falling := false
 var terminalVelocity := 1000
+var direction : float = 0.0
 @export var jumpStrength := 0
 @export var slideFriction := 0.0
 @export var slideSpeed := 0
-@export var walkFriction := 0.0
+@export var walkFriction := 10000.0
 
 #Player inputs and controls
 func _physics_process(delta: float) -> void:
 	countJumps()
 	applyFriction()
-	# Applying Gravity
+#region Applying Gravity
 	if not is_on_floor() && velocity.y < terminalVelocity:
 		velocity.y += getGravity() * delta
-			
-	# Walk Control
-	var direction := Input.get_axis("Move Left", "Move Right")
+#endregion
+#region Movement
+#region Walk Control
+	direction = Input.get_axis("Move Left", "Move Right")
 	if direction && velocity.x < walkSpeed:
 		velocity.x = direction * walkSpeed
 		sliding = false
-		
-	# Slide Control
+#endregion
+#region Slide Control
 	if Input.is_action_just_pressed("Slide"):
 		if is_on_floor() && abs(velocity.x) >= walkSpeed:
 			velocity.x += slideSpeed
 			sliding = true
 			print("sliding")
-			
-	# Jump Control
+#endregion
+#region Jump Control
 	if Input.is_action_just_pressed("Jump"):
 		if is_on_floor():
 			jumpCounter = 0
@@ -46,11 +48,12 @@ func _physics_process(delta: float) -> void:
 			jumpCounter += 1
 			
 			print("jumping")
-			
+#endregion
+#endregion
 	move_and_slide()
-	
 	# End of Physics Process Loop
 	
+#region getGravity() -> int
 func getGravity() -> int:
 	if velocity.y >= 0:
 		jumping = true
@@ -62,13 +65,15 @@ func getGravity() -> int:
 	else:
 		return 0
 	# End of getGravity Function
-		
+#endregion
+#region countJumps()
 func countJumps():
 	if is_on_floor():
 		falling = false
 		jumpCounter = 0
 	# End of countJumps Function
-	
+#endregion
+#region getFriction() -> float
 func getFriction() -> float:
 	if sliding:
 		print("slideFriction")
@@ -79,9 +84,10 @@ func getFriction() -> float:
 	else:
 		print("No Friction")
 		return 0.0
-	# End of getFriction Function
-	
+#endregion
+#region applyFriction()
 func applyFriction():
 	if is_on_floor:
 		velocity.x = move_toward(velocity.x, 0.0, getFriction())
 		#print("applying friction")
+#endregion
